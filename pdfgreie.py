d48936a2e38be1c3bf9a228524dfa15baaa4f0a5
Stian Teien <stian.teien@gmail.com>
2021-10-22 13:18:44 +0200

    lagd ferdig og noen py fil som håndterer pdf2

---
+++ pdfgreie.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 22 13:16:02 2021
+
+@author: Stian
+"""
+
+from PyPDF2 import PdfFileWriter, PdfFileReader
+input_pdf = PdfFileReader("Databehandling.pdf")
+output = PdfFileWriter()
+output.addPage(input_pdf.getPage(0))
+with open("first_page.pdf", "wb") as output_stream:
+    output.write(output_stream)
